1a9c6c296e8c9dd8ab573c1f96f8107f8e1071fc
Valeriameuf <130457046+Valeriameuf@users.noreply.github.com>
2023-12-25 12:13:02 +0200

    Add files via upload

---
+++ lesson_16/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("textarea, input").forEach(function (e) {
+    if (e.value === "")
+      e.value = window.sessionStorage.getItem(e.name, e.value);
+    e.addEventListener("input", function () {
+      window.sessionStorage.setItem(e.name, e.value);
+    });
+  });
+});
