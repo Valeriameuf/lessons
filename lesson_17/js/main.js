a9b2eb3401c2c823b80b863f262b91677069f2c3
Valeriameuf <130457046+Valeriameuf@users.noreply.github.com>
2024-01-06 17:15:12 +0200

    Add files via upload

---
+++ lesson_17/js/main.js
@@ -0,0 +1,12 @@
+const circle = document.querySelector(".progress__circle");
+const radius = circle.r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = circumference;
+
+function setProgress(percent) {
+  const offset = circumference - (percent / 100) * circumference;
+  circle.style.strokeDashoffset = offset;
+}
+setProgress(85);
